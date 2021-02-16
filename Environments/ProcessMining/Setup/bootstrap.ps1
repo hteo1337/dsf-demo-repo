5ef8a94e5321ae1eba04a1f575065f118235531f
HTeo <hteo1337@gmail.com>
2021-02-16 09:53:05 +0200

    ProcessMining

---
+++ Environments/ProcessMining/Setup/bootstrap.ps1
@@ -0,0 +1,43 @@
+[CmdletBinding()]
+
+param(
+    [Parameter()]
+    [string] $licenseCode
+)
+
+#Enable TLS12
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+function Main() {
+    $siteName = 'UiPathProcessMining'
+    $dnsName = $env:computername 
+
+    # create the ssl certificate
+    $newCert = New-SelfSignedCertificate  -DnsName $dnsName -CertStoreLocation cert:\LocalMachine\My
+
+    # get the web binding of the site
+    $binding = Get-WebBinding -Name $siteName -Protocol "https"
+
+    # set the ssl certificate
+    $binding.AddSslCertificate($newCert.GetCertHashString(), "my")
+
+
+    Start-Process "iisreset.exe" -NoNewWindow 
+
+    if ($licenseCode) {
+        licensePM -license $licenseCode
+    }
+}
+function licensePM() {
+    param(
+        [Parameter()]
+        [string] $license
+    )
+
+    Start-Process -FilePath 'C:\Windows\System32\cmd.exe' -ArgumentList '/c', 'C:\UiPathProcessMining\builds\magnaview.bat', "  -license -activate $license"
+}
+
+Main
