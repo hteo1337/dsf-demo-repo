512f880165fe23b1e2b3e853f052ec3bb5a0892c
HTeo <hteo1337@gmail.com>
2021-02-22 21:29:53 +0200

    update GSE image

--- Scripts/bootstrap-gse.ps1
+++ Scripts/bootstrap-gse.ps1
@@ -1,11 +1,17 @@
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory = $false)]
-    [string] $modernFolderRobotsExe="C:\Temp\ModernRobotProvisioning.exe"
+    [string] $modernFolderRobotsExe="C:\Temp\robotProvisioner.exe",
+    [Parameter(Mandatory = $false)]
+    [string] $username,
+    [Parameter(Mandatory = $false)]
+    [string] $domain="DSF",
+    [Parameter(Mandatory = $false)]
+    [string] $robotName
 
 )
 if (Test-Path -Path $modernFolderRobotsExe) {
-    & $modernFolderRobotsExe
+    & $modernFolderRobotsExe -u $username -d $domain -r $robotName
 }
 
 Remove-Item "C:\Temp" -Force -Recurse
