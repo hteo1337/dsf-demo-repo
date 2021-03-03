b8979407cfdda3f7fe84350db3309a4c8c37f4a6
HTeo <hteo1337@gmail.com>
2021-03-03 21:37:52 +0200

    Update bootstrap-gse.ps1

--- Environments/GSE/Setup/bootstrap-gse.ps1
+++ Environments/GSE/Setup/bootstrap-gse.ps1
@@ -16,7 +16,58 @@ Add-MpPreference -ExclusionPath "C:\Temp"
 
 if (Test-Path -Path $modernFolderRobotsExe) {
     
-    & $modernFolderRobotsExe -u $username  -r $robotName -d $domain
+    & $modernFolderRobotsExe -u $username  -r $robotName  -d $domain
 }
 
 # Remove-Item "C:\Temp" -Force -Recurse
+
+
+
+function Register-EventScript {
+    param (
+        [string] $eventToRegister, # Either Startup or Shutdown
+        [string] $pathToScript,
+        [string] $scriptParameters
+    )
+    
+    $path = "$ENV:systemRoot\System32\GroupPolicy\Machine\Scripts\$eventToRegister"
+    if (-not (Test-Path $path)) {
+        # path HAS to be available for this to work
+        New-Item -path $path -itemType Directory
+    }
+
+    # Add script to Group Policy through the Registry
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\$eventToRegister\0\0",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\$eventToRegister\0\0" |
+    ForEach-Object { 
+        if (-not (Test-Path $_)) {
+            New-Item -path $_ -force
+        }
+    }
+
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\$eventToRegister\0",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\$eventToRegister\0" |
+    ForEach-Object {
+        New-ItemProperty -path "$_" -name DisplayName -propertyType String -value "Local Group Policy" -force
+        New-ItemProperty -path "$_" -name FileSysPath -propertyType String -value "$ENV:systemRoot\System32\GroupPolicy\Machine"  -force
+        New-ItemProperty -path "$_" -name GPO-ID -propertyType String -value "LocalGPO" -force
+        New-ItemProperty -path "$_" -name GPOName -propertyType String -value "Local Group Policy" -force
+        New-ItemProperty -path "$_" -name PSScriptOrder -propertyType DWord -value 2  -force
+        New-ItemProperty -path "$_" -name SOM-ID -propertyType String -value "Local" -force
+    }
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\$eventToRegister\0\0",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\$eventToRegister\0\0" |
+    ForEach-Object {
+        New-ItemProperty -path "$_" -name Script -propertyType String -value $pathToScript -force 
+        New-ItemProperty -path "$_" -name Parameters -propertyType String -value $scriptParameters -force
+        New-ItemProperty -path "$_" -name IsPowershell -propertyType DWord -value 1 -force
+        New-ItemProperty -path "$_" -name ExecTime -propertyType QWord -value 0 -force
+    }
+}
+
+# get crrentlocation so we can form the full path to the script to register
+# $currentLocation = $PSScriptRoot
+
+# register the script twice
+# Register-EventScript -eventToRegister "Startup" -pathToScript "$currentLocation\ScriptToRun.ps1" -scriptParameters "OnStartup"
+Register-EventScript -eventToRegister "Shutdown" -pathToScript "C:\Temp\ModernRobotProvisioning.exe -dp" -scriptParameters "OnShutdown"
